26f965b631fd35d08f3acd90cf36d069f67f9ffd
황성우 <hsw0194@hanyang.ac.kr>
2019-03-25 13:37:28 +0900

    Add interactive mode

--- extra/proj_shell/shell.c
+++ extra/proj_shell/shell.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <unistd.h>
 
 #define BUF_SIZE 2097152
 #define NUM_CMD 50
@@ -10,87 +11,67 @@
 #define CMD_TOKEN ";"
 #define CMD_OPTION_TOKEN " "
 
-int count_char(char*,char);
 
-int main(int argc,char * argv[]){
-  if(argc ==1)
-  {
-    while(1){
-      printf("> ");
-      char statement[100];
-      fgets(statement,100,stdin);
-      
-
-      const char s[2]=";";
-
-      char *temp;
-      temp=strtok(statement,CMD_TOKEN);
-      printf("%s l28\n",temp);
-
-      //ok
-
-      char cmd_sets[50][50]={};
-      printf("%s\n",cmd_sets[0]);
-
-      int i=0;
-      printf("here l42\n");
-      while(temp!=NULL){
-        strcpy(cmd_sets[i],temp);
-        temp=strtok(NULL,CMD_TOKEN);
-        i++;
-      }
-
-      int j=0;
-      int k=1;
-      int num_cmd=0;
-      char *cmd_sets_options[50][100]={};
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		while (1) {
+			printf("> ");
+			char statement[BUF_SIZE];
+			fgets(statement, BUF_SIZE, stdin);
 
 
-      while(j<NUM_CMD&&cmd_sets_options[j]!=NULL){
-        char *temp2=strtok(cmd_sets[j],CMD_OPTION_TOKEN);
-        strcpy(cmd_sets_options[j][0],temp2);
-       while(temp2!=NULL){
-         temp2=strtok(NULL,CMD_OPTION_TOKEN);
-         strcpy(cmd_sets_options[j][k++],temp2);
-         printf("%s\n",cmd_sets_options[j][k]);
-       }
-       num_cmd++;
-       cmd_sets_options[j++][49]=NULL;
-      }
+			char *temp;
+			temp = strtok(statement, CMD_TOKEN);
 
-      int p=0;
-      for(;p<num_cmd;p++){
-      pid_t child_pid;
-      int child_status;
+			//ok
 
-      child_pid=fork();
-      if(child_pid==0){
-        // child process doing work
-        execvp(cmd_sets_options[p][0],cmd_sets_options[p]);
+			char *cmd_sets[50] = {};
 
-        perror("Unknown Command\n");
-      }
-      else
-      {
-        while(wait(&child_status)<0);
-      }
+			int i = 0;
 
-      }
+			while (temp != NULL) {
+				cmd_sets[i] = temp;
+				temp = strtok(NULL, CMD_TOKEN);
+				i++;
+			}
 
-    }
-  }
-  else
-  {
-    // batch mode
-  }
+
+			char *temp3;
+			for (int k = 0; k < 50 && cmd_sets[k] != NULL; k++) {
+				int ii = 0;
+
+				// replace new line into null
+				char *p = strchr(cmd_sets[k], '\n');
+				if (p != NULL) *p = '\0';
+
+
+				char *cmd_options[6] = {};
+
+				temp3 = strtok(cmd_sets[k], CMD_OPTION_TOKEN);
+				while (temp3 != NULL) {
+					cmd_options[ii++] = temp3;
+					temp3 = strtok(NULL, CMD_OPTION_TOKEN);
+//					*p = "";
+				}
+				//execvp section
+				pid_t child_pid;
+				int child_status;
+
+				child_pid = fork();
+				if (child_pid == 0) {
+					execvp(cmd_options[0], cmd_options);
+
+					perror("Unknown Command\n");
+				} else {
+					while (wait(&child_status) < 0);
+
+					printf("Finished\n");
+				}
+			}
+
+		}
+	} else {
+		// batch mode
+	}
 }
 
-int count_char(char* line,char target){
-  int i=0;
-  int count=0;
-  while(line[i]!='\0'){
-    if(line[i++]==target)
-      count++;
-  }
-  return count;
-}
