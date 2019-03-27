2707f0d4cefbc034e51894bdf050aa4c7632386d
황성우 <hsw0194@hanyang.ac.kr>
2019-03-28 01:19:45 +0900

    Add batch mode

--- extra/proj_shell/shell.c
+++ extra/proj_shell/shell.c
@@ -4,18 +4,28 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
-#define BUF_SIZE 2097152
+
+#define BUF_SIZE 1000
 #define LIMIT_NUM_CMD 50
 #define LIMIT_NUM_CMD_OPTION 10
+#define ERR_CODE -1
 #define CMD_TOKEN ";"
 #define CMD_OPTION_TOKEN " "
 
 
-int main(int argc, char *argv[]) {
-	if (argc == 1) {
-		while (1) {
-      char *all_cmd_sets[LIMIT_NUM_CMD][LIMIT_NUM_CMD_OPTION]={};
+int main(int argc, char *argv[])
+{
+
+
+	if (argc == 1)
+	{
+		while (1)
+		{
+			int num_of_cmd = 0;
+			int k = 0;
+			char *all_cmd_sets[LIMIT_NUM_CMD][LIMIT_NUM_CMD_OPTION] = {};
 			printf("> ");
 			char raw_input[BUF_SIZE];
 
@@ -23,6 +33,11 @@ int main(int argc, char *argv[]) {
 			// ctrl-D check
 			if (fgets(raw_input, BUF_SIZE, stdin) == NULL)
 				_exit(0);
+
+			// replace new line into null
+			char *newline_pointer = strchr(raw_input, '\n');
+			if (newline_pointer != NULL) *newline_pointer = '\0';
+
 
 			char *temp_for_tokenized_cmd;
 			temp_for_tokenized_cmd = strtok(raw_input, CMD_TOKEN);
@@ -33,67 +48,169 @@ int main(int argc, char *argv[]) {
 
 			int i = 0;
 
-			while (temp_for_tokenized_cmd != NULL) {
+			while (temp_for_tokenized_cmd != NULL)
+			{
 				cmd_sets[i] = temp_for_tokenized_cmd;
 				temp_for_tokenized_cmd = strtok(NULL, CMD_TOKEN);
 				i++;
+//				if (temp_for_tokenized_cmd != NULL)num_of_cmd++;
 			}
 
 
-			char *temp_for_separted_cmd;
-			for (int k = 0; k < LIMIT_NUM_CMD && cmd_sets[k] != NULL; k++) {
+			char *temp_for_separated_cmd;
+			for (k = 0; k < LIMIT_NUM_CMD && cmd_sets[k] != NULL; k++)
+			{
 				int ii = 0;
-
-				// replace new line into null
-				char *newline_pointer = strchr(cmd_sets[k], '\n');
-				if (newline_pointer != NULL) *newline_pointer = '\0';
-
 
 				char *cmd_with_options[LIMIT_NUM_CMD_OPTION] = {};
 
-				temp_for_separted_cmd = strtok(cmd_sets[k], CMD_OPTION_TOKEN);
-				while (temp_for_separted_cmd != NULL) {
-					cmd_with_options[ii++] = temp_for_separted_cmd;
-					temp_for_separted_cmd = strtok(NULL, CMD_OPTION_TOKEN);
+				temp_for_separated_cmd = strtok(cmd_sets[k], CMD_OPTION_TOKEN);
+				while (temp_for_separated_cmd != NULL)
+				{
+					cmd_with_options[ii++] = temp_for_separated_cmd;
+					temp_for_separated_cmd = strtok(NULL, CMD_OPTION_TOKEN);
 				}
-				//execvp section
-				pid_t child_pid;
-				int child_status;
 
-				if (cmd_with_options[0] != NULL && !strcmp(cmd_with_options[0], "quit")) {
+
+				if (cmd_with_options[0] != NULL && !strcmp(cmd_with_options[0], "quit"))
+				{
 					_exit(0);
 				}
 
-        for(int i=0;i<LIMIT_NUM_CMD;i++){
-          for(int j=0;j<LIMIT_NUM_CMD_OPTION&&
-              cmd_with_options[j]!=NULL;j++)
-          {
-            all_cmd_sets[i][j]=cmd_with_options[j];
-          }
-        }
+
+				for (int j = 0; j < LIMIT_NUM_CMD_OPTION &&
+				                cmd_with_options[j] != NULL; j++)
+				{
+					all_cmd_sets[k][j] = cmd_with_options[j];
+				}
+			}
 
 
-				/*child_pid = fork();
-				if (child_pid == 0) {
-					// child process section
-					// if command is quit, end this child process.
 
-					execvp(cmd_with_options[0], cmd_with_options);
-
+			//execvp section
+			pid_t child_pid, wait_pid;
+			int status_child = 0;
+			for (int id = 0; id < k; id++)
+			{
+				if ((child_pid = fork()) == 0)
+				{
+					//child section
+					execvp(all_cmd_sets[id][0], all_cmd_sets[id]);
 					perror("Unknown Command\n");
-				} else {
-					while (wait(&child_status) < 0);
+				}
+			}
 
-					printf("Finished\n");
-				}*/
+			int wait_cnt = 0;
+			printf("%d\n\n", k);
+			while ((wait_pid = wait(&status_child)) > 0
+			       && ++wait_cnt < k);
+
+			if (errno != 0)
+			{
+				printf("errno is %d\n\n\n", errno);
 			}
 		}
 
 
-
-
-	} else {
+	}
+	else
+	{
 		// batch mode
+		FILE *batch_file = fopen(argv[1], "r");
+
+		if (batch_file == NULL)
+		{
+			_exit(0);
+		}
+		else
+		{
+			int num_of_cmd = 0;
+
+			while (!feof(batch_file))
+			{
+				char batch_line[BUF_SIZE] = {};
+				if (fgets(batch_line, BUF_SIZE, batch_file) == NULL)
+				{
+					exit(0);
+				}
+
+				printf("Your current command is %s", batch_line);
+				printf("Working...\n");
+
+				char *newline_pointer = strchr(batch_line, '\n');
+				if (newline_pointer != NULL) *newline_pointer = '\0';
+//				 replace \n into null char
+
+				int num_of_cmd_in_oneline = 0;
+
+
+				// semicolon tokenize
+				char *temp_for_tokenized_cmd;
+				temp_for_tokenized_cmd = strtok(batch_line, CMD_TOKEN);
+
+				char *cmd_sets[LIMIT_NUM_CMD] = {};
+
+				while (temp_for_tokenized_cmd != NULL)
+				{
+					cmd_sets[num_of_cmd_in_oneline++] = temp_for_tokenized_cmd;
+					temp_for_tokenized_cmd = strtok(NULL, CMD_TOKEN);
+				}
+
+				printf("num of cmd in oneline %d\n", num_of_cmd_in_oneline);
+
+				char *temp_for_separated_cmd;
+				char *cmd_with_options[LIMIT_NUM_CMD][LIMIT_NUM_CMD_OPTION] = {};
+				for (int p = 0; p < num_of_cmd_in_oneline; p++)
+				{
+					temp_for_separated_cmd = strtok(cmd_sets[p], CMD_OPTION_TOKEN);
+					for (int temp_target = 0; temp_for_separated_cmd != NULL; temp_target++)
+					{
+						cmd_with_options[p][temp_target] = temp_for_separated_cmd;
+						temp_for_separated_cmd = strtok(NULL, CMD_OPTION_TOKEN);
+					}
+				}
+
+
+				pid_t child_pid, wait_pid;
+				int status_child = 0;
+
+				for (int fork_count = 0; fork_count < num_of_cmd_in_oneline; fork_count++)
+				{
+					if (!strcmp(cmd_with_options[fork_count][0], "quit"))
+					{
+						//quit command
+						printf("quit command encountered\n");
+						_exit(0);
+					}
+					if ((child_pid = fork()) == 0)
+					{
+						// child section
+
+						int execvp_return = execvp(cmd_with_options[fork_count][0],
+						                           cmd_with_options[fork_count]);
+						if (execvp_return == ERR_CODE)
+						{
+							printf("error occured during execvp\n");
+							printf("Unknown Command\n");
+
+							_exit(errno);
+						}
+					}
+				}
+
+				int wait_cnt = 0; // count how many times wait function called
+
+				while ((wait_pid = wait(&status_child)) > 0
+				       && ++wait_cnt < num_of_cmd_in_oneline)
+					if (errno != 0)
+					{
+						printf("errno is %d\n\n\n", errno);
+					}
+			}
+
+		}
+
+
 	}
 }
 
