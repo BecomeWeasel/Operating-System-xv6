6edb77887f8b6701db863dd49b4be796bdd1aa14
황성우 <hsw0194@hanyang.ac.kr>
2019-04-27 19:00:42 +0900

    Modify on defs.h

--- xv6-public/defs.h
+++ xv6-public/defs.h
@@ -120,7 +120,9 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+#ifdef MLFQ_SCHED
 struct proc*    getprocbyid(int pid);
+#endif
 
 // swtch.S
 void            swtch(struct context**, struct context*);
