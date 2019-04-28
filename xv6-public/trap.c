2abff2c2410b8fb23b427af961d5136d3198d3cd
황성우 <hsw0194@hanyang.ac.kr>
2019-04-28 13:52:15 +0900

    Mofiy timer condition on trap.c

--- xv6-public/trap.c
+++ xv6-public/trap.c
@@ -125,7 +125,6 @@ trap(struct trapframe *tf)
     }
 #elif MLFQ_SCHED
 	if(myproc()&&myproc()->state==RUNNING &&
-	  tf->trapno== T_IRQ0+IRQ_TIMER&&
 	  myproc()->lev==0 &&
 	  myproc()->rtime==4){ // L0 RR timer
 
@@ -134,15 +133,13 @@ trap(struct trapframe *tf)
 
 	  }
 	if(myproc()&&myproc()->state==RUNNING &&
-	  tf->trapno== T_IRQ0+IRQ_TIMER&&
 	  myproc()->lev==1 &&
 	  myproc()->rtime==8) { // L1 RR timer + reverse againg
 	  if(myproc()->priority>0)
 	    myproc()->priority--;
 	  yield();
 	  }
-  if(tf->trapno==T_IRQ0+IRQ_TIMER&&
-    ticks%100==0) {// 100ticks 마다 boosting
+  if(ticks%100==0) {// 100ticks 마다 boosting
       priboosting();
   }
 #endif
